912ac47d75973ca5cc8d88ce7af7c217bac880bb
colinnguyen95 <colinnguyen@Colins-iMac.local>
2019-04-29 01:18:16 -0700

    adding JWT and reddis

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const morgan = require('morgan');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
-const morgan = require('morgan');
+const auth = require('./controllers/authorization');
 
+//Database Setup
 const db = knex({
   client: 'pg',
   connection: process.env.POSTGRES_URI
@@ -17,17 +19,27 @@ const db = knex({
 
 const app = express();
 
-app.use(morgan('combined'))
-app.use(cors())
+const whitelist = ['http://localhost:3001']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(morgan('combined'));
+app.use(cors(corsOptions))
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send("ITS WORKING") })
 app.post('/signin', signin.signinAuthentication(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.post('/profile/:id', (req, res) => { profile.handleProfileUpdate(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.get('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileGet(req, res, db)})
+app.post('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileUpdate(req, res, db)})
+app.put('/image', auth.requireAuth, (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', auth.requireAuth, (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(3000, ()=> {
   console.log('app is running on port 3000');
